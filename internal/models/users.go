d12f449ea7d60e2d82712c352f4a4c1e8dd009d0
Vlad Bulgakov <stick.qwe@gmail.com>
2021-06-20 16:57:05 +0300

    wip user models

--- internal/models/users.go
+++ internal/models/users.go
@@ -5,8 +5,11 @@ import "gorm.io/gorm"
 // User ... User Database Model
 type User struct {
 	gorm.Model
-	Email  string  `json:"email" gorm:"unique;not null"`
-	Wallet float64 `json:"wallet"`
+	Email     string `json:"email" gorm:"unique;not null"`
+	Password  string `json:"password" gorm:"not null"`
+	FirstName string `json:"first_name" gorm:"varchar(255);not null"`
+	LastName  string `json:"last_name" gorm:"varchar(255);not null"`
+	Status    bool   `json:"status" gorm:"not null;default:false"`
 }
 
 //MoneyTransfer --- MoneyTransfer Struct
@@ -14,4 +17,9 @@ type MoneyTransfer struct {
 	Receiver uint    `json:"receiver"`
 	Giver    uint    `json:"giver"`
 	Amount   float64 `json:"amount"`
+}
+
+type UserRole struct {
+	UserId int `json:"user_id"`
+	RoleId int `json:"role_id"`
 }
